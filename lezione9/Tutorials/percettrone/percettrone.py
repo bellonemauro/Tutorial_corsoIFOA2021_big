72ed39ec8783708b70e66527a4a32879e357aaff
Mauro Bellone <bellonemauro@gmail.com>
2021-09-19 11:30:08 +0200

    percettrone

---
+++ lezione9/Tutorials/percettrone/percettrone.py
@@ -0,0 +1,181 @@
+#  +---------------------------------------------------------------------------+
+#  |                                                                           |
+#  |  IFOA2021 - BIG DATA e Analisi dei Dati                                   |
+#  |  Tutorial : Percettrone                                                   |
+#  |                                                                           |
+#  |  Autore: Mauro Bellone                                                    |
+#  |  Released under BDS License                                               |
+#  +---------------------------------------------------------------------------+ 
+
+# importo librerie standard
+import numpy as np
+
+class percettroneSemplice():
+    """
+    Implementazione classe percetrone semplice basato su numpy
+
+        
+
+    Esempio d'uso 
+    --------
+    >>> from sklearn.datasets import make_blobs
+    >>> percettrone_semplice = percettroneSemplice(_num_input=num_input, _num_iterazioni = 10000, _learning_rate =0.001) 
+    >>> percettrone_semplice.ottimizza(dati_train, annotazioni_train)
+    >>> annotazioni_pred = percettrone_semplice.predict(dati)
+
+    """
+
+    def __init__(self, _num_input, _num_iterazioni, _learning_rate):
+        """
+        Costruttore dell'oggetto percettrone.
+        Inizia le variabili membro della classe e include l'inizializzazione random dei pesi
+
+        Parametri:
+        ----------
+            _num_input (int): Numero di input
+            _num_iterazioni (int): numero di iterazioni
+            _learning_rate (int): learning rate
+        """
+        print("Istanzio la classe percettrone  ")
+        self.num_input = _num_input
+        self.learning_rate = _learning_rate
+        self.num_iterazioni = _num_iterazioni
+        self.costo_corrente = np.inf
+        self.param_dict = []
+        self.grad_dict = []
+        self.costi = []
+
+        self.pesi, self.bias = self.inizializzazione_pesi(self.num_input)
+        print("I pesi random generati sono: ", self.pesi)
+        print("I bias random generati sono: ", self.bias)
+
+    # Definisco la funzione sigmoide
+    def sigmoide(self, _a):
+        """
+        Semplice implementazione della funzione di attivazione sigmoide 
+
+        Parametri: 
+        ----------
+        _a (float): valore nel quale calcolare la funzione di attivazione 
+
+        Ritorna:
+        ----------
+            valore della sigmoide in _a
+        """
+        return 1/(1 + np.exp(-_a))
+
+    
+    def inizializzazione_pesi(self, _dim):
+        """
+        Funzione di inizializzazione random dei pesi 
+
+        Parametri: 
+        ----------
+        _dim (int): numero di pesi da inizializzare 
+
+        Ritorna:
+        ----------
+        pesi (array[_dim]): array di valori float della dimensione in input _dim
+        bias (float): singolo valore float 
+        """
+        # in questo caso stiamo usando una distribuzione normale per generare i pesi iniziali
+        pesi = np.random.normal((_dim,1)) 
+        bias = 0  # NOTA: tutti i bias sono inizializzati a 0
+        return pesi, bias
+ 
+
+    def propaga(self, _input, _output):
+        """
+        Funzione di propagazione dell'errore, esegue i passi avanti e indietro
+        per ri-settare i pesi del percettrone ad ogni iterazione
+
+        Parametri: 
+        ----------
+        _input (array[float]): dati sui quali effettuare il training
+        _annotazioni (array[float]): annotazioni relative ai dati in input
+        
+        """
+        # propagazione in avanti
+        m = _input.shape[1]
+        attivazione = self.sigmoide(np.dot(self.pesi.T, _input) + self.bias)
+
+        # calcolo la funzione di costo rappresentata da una cross entropy
+        self.costo_corrente = (- 1 / m) * np.sum(_output * np.log(attivazione) + (1 - _output) * (np.log(1 - attivazione)))
+        # FINE propagazione in avanti
+        
+        # Propagazione all'indietro
+        d_pesi = (1/m)*np.dot(_input, (attivazione-_output).T)
+        d_bias = (1/m)*np.sum(attivazione-_output)
+        # FINE: propagazione all'indietro
+        
+        # controllo che le dimensioni e le forme siano corrette
+        assert(d_pesi.shape == self.pesi.shape)
+        assert(d_bias.dtype == float)
+        self.costo_corrente = np.squeeze(self.costo_corrente)
+        assert(self.costo_corrente.shape == ())
+        
+        # struttura dati che salva le derivate dei gradienti 
+        self.grad_dict = {"d_p":d_pesi , "d_b":d_bias}
+        
+  
+    def ottimizza(self, _input, _annotazioni):
+        """
+        Ottimizzatore del percettrone basato sul gradient descent. 
+        Questa funzione non ritorna nulla, i pesi sono aggiornati nelle variabili membro della classe percettrone
+        i valori sono accessibili tramite self.pesi, self.bias
+
+        Parametri: 
+        ----------
+        _input (array[float]): dati sui quali effettuare il training
+        _annotazioni (array[float]): annotazioni relative ai dati in input
+        
+        """
+        self.costi = [] # deve essere azzerata ogni volta che inizio l'ottimizzatore
+        for i in range(self.num_iterazioni):
+            
+
+            self.propaga(_input, _annotazioni)
+            d_pesi = self.grad_dict["d_p"]
+            d_bias = self.grad_dict["d_b"]
+            
+            #Gradient Descent - equazione destrtturata
+            self.pesi = self.pesi - self.learning_rate * d_pesi
+            self.bias = self.bias - self.learning_rate * d_bias
+            #Gradient Descent
+            
+            if i % 1000 == 0:   # stampo il costo solo ogni 1000 iterazioni
+                self.costi.append(self.costo_corrente)
+                print ("Costo dopo %i iterazioni = %f" % (i, self.costo_corrente))
+                
+        self.param_dict = {"p":self.pesi, "b":self.bias}
+        self.grad_dict = {"d_p":d_pesi, "d_b":d_bias}
+        
+        
+
+    # Funzione di predizione (inferenza)
+    def predict(self, _input):
+        """
+        Funzione di predizione dato, assume che il modello sia già generato, 
+        la dimensione di _input deve avere un numero di colonne pari al numero di pesi generati,
+        mentre non c'è limite al numero di righe
+
+        Parametri: 
+        ----------
+        _input (array[float]): dati sui quali effettuare l'inferenza
+
+        Ritorna:
+        ----------
+        output(array[float]): dati in uscita dal percettrone, singolo uscita per ogni ingresso
+        """
+        m = _input.shape[1]
+        output_pred = np.zeros((1,m))
+        self.pesi = self.pesi.reshape(_input.shape[0], 1)
+        
+        attivazione = self.sigmoide(np.dot(self.pesi.T, _input)+ self.bias)
+        
+        for i in range(m):
+            output_pred[0,i] = 1 if attivazione[0,i] > 0.5 else 0
+            
+        assert(output_pred.shape == (1, m))
+            
+        return output_pred
---
+++ lezione9/Tutorials/percettrone/percettroneDEMO.py
@@ -0,0 +1,92 @@
+#  +---------------------------------------------------------------------------+
+#  |                                                                           |
+#  |  IFOA2021 - BIG DATA e Analisi dei Dati                                   |
+#  |  Tutorial : Percettrone                                                   |
+#  |                                                                           |
+#  |  Autore: Mauro Bellone                                                    |
+#  |  Released under BDS License                                               |
+#  +---------------------------------------------------------------------------+ 
+
+
+"""
+Questo tutorial è pensato per mostrare il funzionamento di un semplice percettrone  
+"""
+print(__doc__)
+
+
+# importo librerie standard
+import numpy as np
+import matplotlib.pyplot as plt
+
+# da sklearn importo solo dataset, usato per generare dei dati random e dividere i dati in train e test
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+
+# importo la mia classe
+from percettrone import *
+
+# entry point
+if __name__ == "__main__": 
+
+    # Creo un dataset separabile linearmente con 2 features usando sklearn  
+    dati, annotazioni = datasets.make_blobs(n_samples = 200, n_features=2, centers=2, random_state=50)  
+    #np.random.seed(0) # imposto il random seed per essere sicuro di stare sempre nella stessa condizione
+    #dati, annotazioni = datasets.make_blobs(n_samples = 100, centers = 2, cluster_std=1.5)
+    # Divido il dataset in train e test 
+    dati_train, dati_test, annotazioni_train, annotazioni_test = train_test_split(dati, annotazioni, test_size =0.3, random_state=1)
+
+    # Faccio semplicemente la trasposta dei dati  
+    dati_train = dati_train.T 
+    dati_test = dati_test.T 
+    num_input= dati_train.shape[0]
+    print(num_input)
+
+    plt.scatter(dati_train[0,:],dati_train[1,:], c="blue", alpha=0.6, label="train")# c=annotazioni_train )
+    plt.scatter(dati_test[0,:],dati_test[1,:], c="red", alpha=0.6, label="test")# c=annotazioni_train )
+    #plt.scatter(dati_test[:,0], dati_test[:,1], c=annotazioni_test, s=0.003)
+    #plt.scatter(dati_train[:,0], dati_train[:,1], c=annotazioni, s=0.003)
+    #plt.scatter(dati_test[:,0], dati_test[:,1], c=dict["Y_pred_test"], s=0.003)
+    plt.title(" Dati generati per training e test ")
+    plt.legend(loc="best")
+    plt.xlabel("X1")
+    plt.ylabel("Y2")
+    plt.show()
+
+    # istanziamo la classe percettrone
+    percettrone_semplice = percettroneSemplice(_num_input=num_input, _num_iterazioni = 10000, _learning_rate =0.001)
+
+    # ottimizziamo i parametri in base ai nostri dati di test 
+    percettrone_semplice.ottimizza(dati_train, annotazioni_train)
+    param_dict = percettrone_semplice.param_dict
+    grad_dict = percettrone_semplice.grad_dict
+    loss = percettrone_semplice.costi
+    pesi = percettrone_semplice.pesi
+    bias = percettrone_semplice.bias
+
+    # Plotto la linea di separazione tra le classi
+    valori_x1 = np.linspace(np.min(dati[:,0]),np.max(dati[:,0]),10).reshape((10,1))
+    # i valori in y corrispondono alla retta data dai valori in x nella quale il bias è la quota e i pesi sono il coefficiente angolare 
+    valori_x2 = -(pesi[0]*valori_x1 + bias)/(pesi[1]) 
+
+    annotazioni_pred_train = percettrone_semplice.predict(dati_train)
+    annotazioni_pred_test = percettrone_semplice.predict(dati_test)
+    
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(annotazioni_pred_train - annotazioni_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(annotazioni_pred_test - annotazioni_test)) * 100))
+
+    plt.scatter(dati_train[0,:], dati_train[1,:], c=annotazioni_train, cmap="Dark2", alpha=0.6, label = "train" )
+    plt.scatter(dati_test[0,:], dati_test[1,:], c=annotazioni_test, cmap="Dark2", alpha=1.0, marker = "^", label = "test")
+    plt.plot(valori_x1, valori_x2, c="black", label = "Modello")
+    plt.legend(loc="best")
+    plt.title("Valutazione training e test")
+    plt.xlabel("X1")
+    plt.ylabel("Y2")
+    plt.show()
+
+    plt.plot(loss, c="blue", alpha=0.6)
+    plt.title("Funzione di loss")
+    plt.xlabel("Epoche x 1000")
+    plt.ylabel("Valore della funzione di loss")
+    plt.show()
+  
